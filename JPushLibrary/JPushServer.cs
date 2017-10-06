9b0f385097707a360ec8377ca01585cfca320771
JimmyPun610 <11005408@life.hkbu.edu.hk>
2017-10-06 17:32:12 +0800

    added title field support

--- JPushLibrary/JPushServer.cs
+++ JPushLibrary/JPushServer.cs
@@ -13,44 +13,52 @@ namespace JPushLibrary
         {
             jPushClient = new Jiguang.JPush.JPushClient(appKey, masterSecret);
         }
-        private Jiguang.JPush.Model.IOS createIOSNotification(string message, Dictionary<string, object> extraContent, string sound = "default", string badge = "+1")
+        private Jiguang.JPush.Model.IOS createIOSNotification(string message, string title, Dictionary<string, object> extraContent, string sound = "default", string badge = "+1")
         {
+            JObject jobj = new JObject();
+            jobj.Add("title", title);
+            jobj.Add("body", message);
             return new Jiguang.JPush.Model.IOS
             {
-                Alert = message,
+                Alert = jobj,
                 Badge = badge,
                 Extras = extraContent,
                 Sound = sound,
             };
         }
-        private Jiguang.JPush.Model.Android createAndroidNotification(string message, Enum.AndroidPriority priority, Dictionary<string, object> extraContent, Enum.AndroidAlertType alertType)
+        private Jiguang.JPush.Model.Android createAndroidNotification(string message, string title, Enum.AndroidPriority priority, Dictionary<string, object> extraContent, Enum.AndroidAlertType alertType)
         {
             return new Jiguang.JPush.Model.Android
             {
                 Alert = message,
                 Extras = extraContent,
                 Priority = (int)priority,
-                AlertType = (int)alertType
+                AlertType = (int)alertType,
+                Title = title
             };
         }
-        private Jiguang.JPush.Model.IOS createSlientIOSNotification(string message , Dictionary<string, object> extraContent, string badge = "+1")
+        private Jiguang.JPush.Model.IOS createSlientIOSNotification(string message, string title, Dictionary<string, object> extraContent, string badge = "+1")
         {
+            JObject jobj = new JObject();
+            jobj.Add("title", title);
+            jobj.Add("body", message);
             return new Jiguang.JPush.Model.IOS
             {
                 Extras = extraContent,
                 ContentAvailable = true,
-                Alert = message,
+                Alert = jobj,
                 Badge = badge
             };
         }
-        private Jiguang.JPush.Model.Android createSlientAndroidNotification(string message, Enum.AndroidPriority priority, Dictionary<string, object> extraContent, Enum.AndroidAlertType alertType)
+        private Jiguang.JPush.Model.Android createSlientAndroidNotification(string message, string title, Enum.AndroidPriority priority, Dictionary<string, object> extraContent, Enum.AndroidAlertType alertType)
         {
             return new Jiguang.JPush.Model.Android
             {
                 Extras = extraContent,
                 Priority = (int)priority,
                 AlertType = (int)alertType,
-                Alert = message
+                Alert = message,
+                Title = title
             };
         }
         /// <summary>
@@ -63,7 +71,7 @@ namespace JPushLibrary
         /// <param name="androidAlertType">Indicator the notification method in Android</param>
         /// <param name="platform">indicate target platform</param>
         /// <returns>HttpResponse content</returns>
-        public PushResult Push(string message, Enum.Platform platform = Enum.Platform.iOSProductionAndAndroid, object targetAudience = null, Dictionary<string, object> extraContent = null
+        public PushResult Push(string message, string title , Enum.Platform platform = Enum.Platform.iOSProductionAndAndroid, object targetAudience = null, Dictionary<string, object> extraContent = null
             , Enum.AndroidPriority androidPriority = Enum.AndroidPriority.PRIORITY_DEFAULT
             , Enum.AndroidAlertType androidAlertType = Enum.AndroidAlertType.All)
         {
@@ -88,14 +96,14 @@ namespace JPushLibrary
                 payload.Notification = new Jiguang.JPush.Model.Notification();
                 if(platform == Enum.Platform.Android)
                 {
-                    payload.Notification.Android = createAndroidNotification(message, androidPriority, extraContent, androidAlertType);
+                    payload.Notification.Android = createAndroidNotification(message, title, androidPriority, extraContent, androidAlertType);
                 }else if(platform == Enum.Platform.iOSDevelopment || platform == Enum.Platform.iOSProduction)
                 {
-                    payload.Notification.IOS = createIOSNotification(message, extraContent);
+                    payload.Notification.IOS = createIOSNotification(message, title, extraContent);
                 }else if(platform == Enum.Platform.iOSDevelopmentAndAndroid || platform == Enum.Platform.iOSProductionAndAndroid)
                 {
-                    payload.Notification.IOS = createIOSNotification(message, extraContent);
-                    payload.Notification.Android = createAndroidNotification(message, androidPriority, extraContent, androidAlertType);
+                    payload.Notification.IOS = createIOSNotification(message, title, extraContent);
+                    payload.Notification.Android = createAndroidNotification(message, title, androidPriority, extraContent, androidAlertType);
                 }
                 
                    
@@ -125,7 +133,7 @@ namespace JPushLibrary
         /// <returns>HttpResponse content</returns>
         public PushResult PushSlient(Dictionary<string,object> extraContent, Enum.Platform platform = Enum.Platform.iOSProductionAndAndroid, object targetAudience = null
             , Enum.AndroidPriority androidPriority = Enum.AndroidPriority.PRIORITY_DEFAULT
-            , string message = "", Enum.AndroidAlertType alertType = Enum.AndroidAlertType.LightOnly)
+            , string message = "", string title = "", Enum.AndroidAlertType alertType = Enum.AndroidAlertType.LightOnly)
         {
             PushResult pushResult = null;
             Jiguang.JPush.Model.PushPayload payload = new Jiguang.JPush.Model.PushPayload();
@@ -148,16 +156,16 @@ namespace JPushLibrary
                 payload.Notification = new Jiguang.JPush.Model.Notification();
                 if (platform == Enum.Platform.Android)
                 {
-                    payload.Notification.Android = createSlientAndroidNotification(message, androidPriority, extraContent, alertType);
+                    payload.Notification.Android = createSlientAndroidNotification(message, title, androidPriority, extraContent, alertType);
                 }
                 else if (platform == Enum.Platform.iOSDevelopment || platform == Enum.Platform.iOSProduction)
                 {
-                    payload.Notification.IOS = createSlientIOSNotification(message, extraContent);
+                    payload.Notification.IOS = createSlientIOSNotification(message, title, extraContent);
                 }
                 else if (platform == Enum.Platform.iOSDevelopmentAndAndroid || platform == Enum.Platform.iOSProductionAndAndroid)
                 {
-                    payload.Notification.IOS = createSlientIOSNotification(message, extraContent);
-                    payload.Notification.Android = createSlientAndroidNotification(message, androidPriority, extraContent, alertType);
+                    payload.Notification.IOS = createSlientIOSNotification(message, title, extraContent);
+                    payload.Notification.Android = createSlientAndroidNotification(message, title, androidPriority, extraContent, alertType);
                 }
 
                 var result = jPushClient.SendPush(payload);
